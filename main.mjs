f3899ee72797e9ae1e742e34d0575aa07364baf4
PaxoJW <184737673+PaxoJW@users.noreply.github.com>
2026-02-20 10:03:31 +0100

    Tested the functions
    I tested the functions I wrote so far except for
    the remove(key). They all work and I confirm that,
    even though it doesn't log the message the linked lists
    are correctly inserting new elements.

--- main.mjs
+++ main.mjs
@@ -15,4 +15,11 @@ test.set('jacket', 'blue');
 test.set('kite', 'pink');
 test.set('lion', 'golden');
 
+console.log(test.get('lion')); //golden
+console.log(test.get('apple')); //red
+
+console.log(test.has('dog')); //true
+console.log(test.has('monkey')); //false
+console.log(test.has('hat'));
+
 console.log(test.map);
